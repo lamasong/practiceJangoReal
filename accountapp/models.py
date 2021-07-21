5e05391a601a5ca88dbb1689e9ec5e0395d0e4f4
lamasong <swj8388@gmail.com>
2021-07-21 13:48:55 +0900

    djngo DB initial workstation

--- accountapp/models.py
+++ accountapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 
-class HelloWordl(models.Model):
+class HelloWorld(models.Model):
     test = models.CharField(max_length=255, null=False)
