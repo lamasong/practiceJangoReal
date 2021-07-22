25682e0213b3b84d4642107895aff5ce6671fcaf
lamasong <swj8388@gmail.com>
2021-07-22 15:25:39 +0900

    django course message

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreatView
+from accountapp.views import hello_world, AccountCreatView, AccountDetailView
 
 app_name = "accountapp"
 
@@ -14,4 +14,6 @@ urlpatterns = [
 
     # 클래스 베이스는 다름
     path('create/', AccountCreatView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+
 ]
