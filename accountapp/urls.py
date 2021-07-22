357d4db99b89b3a3954c2c786f4c8a31c4c1f56f
lamasong <swj8388@gmail.com>
2021-07-22 10:41:09 +0900

    django couose 21 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreatView
 
 app_name = "accountapp"
 
+urlpatterns = [
+    # 함수형 베이스 중간에 hello world
+    path('hello_world/', hello_world, name="hello_world"),
 
-urlpatterns = {
-    path('hello_world/', hello_world, name="hello_world")
-}
+    # 클래스 베이스는 다름
+    path('create/', AccountCreatView.as_view(), name='create'),
+]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,14 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-
     if request.method == "POST":
 
         temp = request.POST.get('hello_world_input')
@@ -17,7 +19,17 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreatView(CreateView):
+    model = User
+    form_class = UserCreationForm
+
+    # 성공했을 때 어디로 다시 재연결 한것인가,
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
