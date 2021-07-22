e9b0b1f189d2a38c1063a4a28504241e84019413
lamasong <swj8388@gmail.com>
2021-07-22 11:34:16 +0900

    23

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreatView
@@ -7,6 +8,9 @@ app_name = "accountapp"
 urlpatterns = [
     # 함수형 베이스 중간에 hello world
     path('hello_world/', hello_world, name="hello_world"),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     # 클래스 베이스는 다름
     path('create/', AccountCreatView.as_view(), name='create'),
