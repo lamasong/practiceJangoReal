7f993804255902d6bc7f51987ce470ef38c69970
lamasong <swj8388@gmail.com>
2021-07-23 16:14:24 +0900

    django course 32 commit

--- practiceJangoReal/settings.py
+++ practiceJangoReal/settings.py
@@ -40,7 +40,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -85,7 +84,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'practiceJangoReal.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -95,7 +93,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -115,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -128,7 +124,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- profileapp/models.py
+++ profileapp/models.py
@@ -5,8 +5,8 @@ from django.db import models
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, relate_name="profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
-    image = models.imageField(upload_to='profile/', null=True)
-    nickname = models.charField(max_length = 20, unique=True, null=True)
-    message = models.CharField(max_length = 100, null=True)
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True)
+    message = models.CharField(max_length=100, null=True)
